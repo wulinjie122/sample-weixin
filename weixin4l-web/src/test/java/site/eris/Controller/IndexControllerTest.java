f646920745aedbe85ef38003d288fc5dfe727b45
wulinjie122 <wulinjie122@gmail.com>
2015-11-26 23:13:12 +0800

    初始化项目

---
+++ weixin4l-web/src/test/java/site/eris/Controller/IndexControllerTest.java
@@ -0,0 +1,24 @@
+package site.eris.Controller;
+
+import com.foxinmy.weixin4j.util.MapUtil;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wulinjie on 2015/11/26.
+ */
+public class IndexControllerTest {
+
+	@Test
+	public void testCheckSignature(){
+		List<String> params = new ArrayList<String>();
+		params.add("token");
+		params.add("20151103");
+		params.add("ewewe");
+		String str = MapUtil.toJoinString(params, false, false, null);
+		System.out.println(str);
+	}
+
+}
